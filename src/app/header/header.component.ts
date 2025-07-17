763ecf797740c960e91e6b75282bb8f00f350e6f
unknown <shivakumararadyasm5@gmail.com>
2025-07-17 06:42:57 +0530

    first commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-headerḍ',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+
+export class HeaderComponent { }
